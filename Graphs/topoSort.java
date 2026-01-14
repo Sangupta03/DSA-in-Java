b376627a06f83f7ed71f657b224975062a670247
Sangupta03 <sanjoligupta11d@gmail.com>
2026-01-14 19:50:11 +0530

    Topological sorting using Dfs

---
+++ Graphs/topoSort.java
@@ -0,0 +1,43 @@
+package Graphs;
+import java.util.*;
+class topoSort {
+    public ArrayList<Integer> topologicalSort(int V, int[][] edges) {
+        // code here
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] edge:edges){
+            adj.get(edge[0]).add(edge[1]);
+        }
+        
+        boolean[] vis=new boolean[V];
+        Stack<Integer> stk=new Stack<>();
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,vis,adj,stk);
+            }
+        }
+        
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(!stk.isEmpty()){
+            ans.add(stk.pop());
+        }
+        return ans;
+    }
+    
+    public void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> stk){
+        
+        vis[node]=true;
+        
+        for(int neighbour:adj.get(node)){
+            if(!vis[neighbour]){
+                dfs(neighbour,vis,adj,stk);
+            }
+        }
+        stk.push(node);
+    }
+}
