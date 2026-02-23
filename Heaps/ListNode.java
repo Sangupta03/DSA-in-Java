053a4e55c675673547d620d1369dec195b258801
Sangupta03 <sanjoligupta11d@gmail.com>
2026-02-23 23:48:55 +0530

    Merge k sorted Lists

---
+++ Heaps/ListNode.java
@@ -0,0 +1,34 @@
+package Heaps;
+import java.util.*;
+
+ public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class mergeKsortedList {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+
+        for(ListNode head:lists){
+            if(head!=null) pq.offer(head);
+        }
+
+        
+        ListNode dNode=new ListNode(-1);
+        ListNode temp=dNode;
+        
+        while(!pq.isEmpty()){
+            ListNode nNode=pq.poll();
+            temp.next=nNode;
+            temp=temp.next;
+
+            if(nNode.next!=null) pq.offer(nNode.next);
+        }
+        return dNode.next;
+    }
+}
