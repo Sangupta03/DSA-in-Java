5b81f17e333dc8e7a30e2f4132769cfb28c32d83
Sangupta03 <sanjoligupta11d@gmail.com>
2026-03-04 15:47:45 +0530

    To find the power of K-size Subarrays

---
+++ SlidingWindow/PowerOfKSubarray.java
@@ -0,0 +1,26 @@
+package SlidingWindow;
+
+class PowerOfKSubarray {
+    public int[] resultsArray(int[] nums, int k) {
+        int n=nums.length;
+        int[] res=new int[n-k+1];
+        int cnt=0;
+        if(k==1) return nums;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]+1){
+                cnt++;
+            }else{
+                cnt=0;
+            }
+
+            if(i>=k-1){
+                if(cnt>=k-1){
+                    res[i-k+1]=nums[i];
+                }else{
+                    res[i-k+1]=-1;
+                }
+            }
+        }
+        return res;
+    }
+}
