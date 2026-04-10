2662be5f820702f63e28f2e806ba79a42979d1b2
Sangupta03 <sanjoligupta11d@gmail.com>
2026-04-10 20:23:21 +0530

    Dp problem- to find min path falling sum with variable start and ending points

--- dp/minPathFallSum.java
+++ dp/minPathFallSum.java
@@ -30,3 +30,40 @@ class minPathFallSum {
         return dp[i][j]=Math.min(ldg,Math.min(up,rdg));
     }
 }
+
+class Solution2 {
+    public int minFallingPathSum(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        int[][] dp=new int[m][n];
+
+        for(int j=0;j<n;j++){
+            dp[0][j]=matrix[0][j];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                int ldg=matrix[i][j];
+                int up=matrix[i][j]+dp[i-1][j];
+                int rdg=matrix[i][j];
+                if(j-1>=0){
+                    ldg=matrix[i][j]+dp[i-1][j-1];
+                }else{
+                    ldg=(int) 1e9;
+                }if(j+1<n){
+                    rdg=matrix[i][j]+dp[i-1][j+1];
+                }else{
+                    rdg=(int) 1e9;
+                }
+                dp[i][j]=Math.min(ldg,Math.min(rdg,up));
+            }
+
+        }
+
+        int mini=Integer.MAX_VALUE;
+        for(int j=0;j<n;j++){
+            mini=Math.min(mini,dp[m-1][j]);
+        }
+        return mini;
+    }
+}
