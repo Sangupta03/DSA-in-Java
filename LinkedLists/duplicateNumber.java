4aa19ae79e54bffe055cce141171dd5f0d46ddd0
Sangupta03 <sanjoligupta11d@gmail.com>
2026-05-09 18:33:24 +0530

    Duplicate number using cylce detetction

---
+++ LinkedLists/duplicateNumber.java
@@ -0,0 +1,19 @@
+class duplicateNumber {
+    public int findDuplicate(int[] nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+//detect a loop
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(fast==slow) break;
+        }
+// find start of the loop
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+}
