1edf97c3aa69b7a590615f5e933b983e87a3bcfb
Sangupta03 <sanjoligupta11d@gmail.com>
2026-05-10 10:25:07 +0530

    Add two numbers

---
+++ LinkedLists/add2Numbers.java
@@ -0,0 +1,25 @@
+class add2Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode t1=l1;
+        ListNode t2=l2;
+        int carry=0;
+        ListNode dNode=new ListNode(-1);
+        ListNode temp=dNode;
+        while(t1!=null || t2!=null){
+            int sum=carry;
+            if(t1!=null) sum+=t1.val;
+            if(t2!=null) sum+=t2.val;
+            ListNode newNode=new ListNode(sum%10);
+            carry=sum/10;
+            temp.next=newNode;
+            temp=temp.next;
+            if(t1!=null) t1=t1.next;
+            if(t2!=null) t2=t2.next;
+        }
+        if(carry==1){
+            ListNode tail=new ListNode(1);
+            temp.next=tail;
+        }
+        return dNode.next;
+    }
+}
