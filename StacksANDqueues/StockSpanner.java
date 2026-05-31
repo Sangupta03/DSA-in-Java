d69a7f53b2fafa7d28e4d65ee4446a8cf16a8b82
Sangupta03 <sanjoligupta11d@gmail.com>
2026-05-31 18:55:42 +0530

    Find stock span

---
+++ StacksANDqueues/StockSpanner.java
@@ -0,0 +1,36 @@
+package StacksANDqueues;
+
+class Value{
+    int idx;
+    int val;
+    Value(int idx,int val){
+        this.idx=idx;
+        this.val=val;
+    }
+}
+class StockSpanner {
+
+    public Stack<Value> stk;
+    public int idx;
+    public StockSpanner() {
+     stk=new Stack<>();
+     idx=-1;  
+    }
+    
+    public int next(int price) {
+        idx++;
+        int span;
+
+        while(!stk.isEmpty() && stk.peek().val<=price){
+            stk.pop();
+        }
+
+        if(stk.isEmpty()){
+            span=idx+1;
+        }else{
+            span=idx-stk.peek().idx;
+        }
+        stk.push(new Value(idx,price));
+        return span;
+    }
+}
