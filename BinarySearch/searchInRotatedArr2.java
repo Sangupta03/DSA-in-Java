38720b2b807f79f715493d2588cec2d90e2b53a0
Sangupta03 <sanjoligupta11d@gmail.com>
2026-06-08 16:16:33 +0530

    Find if target exists in rotated array

---
+++ BinarySearch/searchInRotatedArr2.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+class searchInRotatedArr2 {
+    public boolean search(int[] nums, int target) {
+        int left=0;
+        int n=nums.length;
+        int right=n-1;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[left]==target && nums[right]==target){
+                left++;
+                right--;
+            }else if(nums[left]<=nums[mid]){
+                if(nums[left]<= target && target<=nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
