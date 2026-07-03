f2f1c863f84c31b25cb5312a7d94126e0b6922f0
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-03 23:31:10 +0530

    group anagrams together

---
+++ Sliding_window/groupAnagramsQ.java
@@ -0,0 +1,27 @@
+
+package Sliding_window;
+
+import java.util.*;
+class groupAnagramsQ {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List> res=new HashMap<>();
+
+        for(String str: strs){
+            int[] count=new int[26];
+            for(char ch: str.toCharArray()){
+                count[ch-'a']++;
+            }
+            StringBuilder sb=new StringBuilder();
+            for (int i:count){
+                sb.append(i);
+                sb.append('#');
+            }
+            String key=sb.toString();
+            if(!res.containsKey(key)){
+                res.put(key,new ArrayList<String>());
+            }
+            res.get(key).add(str);
+        }
+        return new ArrayList(res.values());
+    }
+}
