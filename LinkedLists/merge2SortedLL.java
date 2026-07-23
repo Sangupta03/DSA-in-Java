06e75d2e181bf248967a0c39788da2cabff41f6d
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-23 15:36:26 +0530

    Merge sorted LL

---
+++ LinkedLists/merge2SortedLL.java
@@ -0,0 +1,31 @@
+class merge2SortedLL {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode l1=list1;
+        ListNode l2=list2;
+
+        ListNode dNode=new ListNode(-1);
+        ListNode temp=dNode;
+
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                temp.next=l1;
+                temp=temp.next;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                temp=temp.next;
+                l2=l2.next;
+            }
+        }
+        if(l1!=null) temp.next=l1;
+        if(l2!=null) temp.next=l2;
+        return dNode.next;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
