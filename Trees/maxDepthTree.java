81ca7629233667171e390a01a64cefab03f3889b
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-25 16:12:41 +0530

    Find maxdepth of BT

---
+++ Trees/maxDepthTree.java
@@ -0,0 +1,23 @@
+package Trees;
+
+class maxDepthTree {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int leftH=1+maxDepth(root.left);
+        int rightH=1+maxDepth(root.right);
+        return Math.max(leftH,rightH);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
