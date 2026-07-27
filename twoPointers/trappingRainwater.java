732e0ee07ae327fcb238d0e33dc14a3be984adc1
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-28 02:23:08 +0530

    Trapping rain water

---
+++ twoPointers/trappingRainwater.java
@@ -0,0 +1,24 @@
+package twoPointers;
+
+class trappingRainwater {
+    public int trap(int[] height) {
+        int leftMax=0;
+        int rightMax=0;
+        int left=0;
+        int right=height.length-1;
+        int water=0;
+        
+        while(left<right){
+            if(height[left]<height[right]){
+                leftMax=Math.max(leftMax,height[left]);
+                water+=leftMax-height[left];
+                left++;
+            }else{
+                rightMax=Math.max(rightMax,height[right]);
+                water+=rightMax-height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+}
