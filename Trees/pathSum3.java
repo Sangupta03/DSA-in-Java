5f864af011c9c3b1613b854dc92b0f6d086081b4
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-27 20:55:37 +0530

    Count all the paths that sum to targetSum

---
+++ Trees/pathSum3.java
@@ -0,0 +1,24 @@
+class pathSum3 {
+    HashMap<Long,Integer> hp=new HashMap<>();
+    int cnt=0;
+    public int pathSum(TreeNode root, int targetSum) {
+        hp.put(0L,1);
+        solve(root,targetSum,0L);
+        return cnt;
+    }
+
+    public void solve(TreeNode node,int targetSum,long curr){
+        if(node==null) return;
+        curr+=node.val;
+        long rem=curr-targetSum;
+
+        if(hp.containsKey(rem)){
+            cnt+=hp.get(rem);
+        }
+        hp.put(curr,hp.getOrDefault(curr,0)+1);
+        solve(node.left,targetSum,curr);
+        solve(node.right,targetSum,curr);
+        hp.put(curr,hp.getOrDefault(curr,0)-1);
+        
+    }
+}
