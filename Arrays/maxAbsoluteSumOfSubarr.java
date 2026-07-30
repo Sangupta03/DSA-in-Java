4c3bf234853c0add51fd1cf471a32a3deb865b9c
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-30 05:33:41 +0530

    Using kadane to find max absolute sum of subarray

---
+++ Arrays/maxAbsoluteSumOfSubarr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class maxAbsoluteSumOfSubarr {
+    public int subarraysDivByK(int[] nums, int k) {
+        int cnt=0;
+        HashMap<Long,Integer> hp=new HashMap<>(); //to store remainders
+        int n=nums.length;
+        hp.put(0L,1);
+        long sum=0;
+
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            long rem=sum%k;
+            if(rem<0) rem+=k;
+            cnt+=hp.getOrDefault(rem,0);
+            hp.put(rem,hp.getOrDefault(rem,0)+1);
+        }
+        return cnt;
+    }
+}
