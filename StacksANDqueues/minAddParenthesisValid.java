f464bd86935dd7b95eb0a1e359251d4d4d0620ca
Sangupta03 <sanjoligupta11d@gmail.com>
2026-07-31 21:57:26 +0530

    Minimum Add to make parenthesis

---
+++ StacksANDqueues/minAddParenthesisValid.java
@@ -0,0 +1,19 @@
+package StacksANDqueues;
+
+class minAddParenthesisValid {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                open++;
+            }else if(open>0 && ch==')'){
+                open--;
+            }else{
+                close++;
+            }
+        }
+        return open+close;
+    }
+}
