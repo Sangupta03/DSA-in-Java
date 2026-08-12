b751f28d3ede9837b61834890753a175c76735e6
Sangupta03 <sanjoligupta11d@gmail.com>
2026-08-13 04:17:43 +0530

    Rat in a maze

---
+++ Recursion/ratInMaze.java
@@ -0,0 +1,40 @@
+package Recursion;
+class Solution {
+    public ArrayList<String> ratInMaze(int[][] maze) {
+        // code here
+        ArrayList<String> ans=new ArrayList<>();
+        int n=maze.length;
+        int m=maze[0].length;
+        boolean[][] vis=new boolean[n][m];
+        solve(0,0,maze,vis,ans,new StringBuilder());
+        return ans;
+    }
+    
+    public void solve(int i,int j,int[][] maze,boolean[][] vis,ArrayList<String> ans,StringBuilder path){
+        int n=maze.length;
+        int m=maze[0].length;
+        
+        if(i==n-1 && j==m-1){
+            ans.add(path.toString());
+            return;
+        }
+        if(i>=n || j>=m || i<0 || j<0 || vis[i][j] || maze[i][j]==0){
+            return;
+        }
+        
+        char[] dir={'D','L','R','U'};
+        int[] dr={1,0,0,-1};
+        int[] dc={0,-1,1,0};
+        
+        vis[i][j]=true;
+        
+        for(int idx=0;idx<4;idx++){
+            path.append(dir[idx]);
+            solve(i+dr[idx],j+dc[idx],maze,vis,ans,path);
+            path.deleteCharAt(path.length()-1);
+        }
+        
+        vis[i][j]=false;
+        
+    }    
+}
