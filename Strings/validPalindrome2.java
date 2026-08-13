602ffee607076e1fc3abde18c593906294ffd71b
Sangupta03 <sanjoligupta11d@gmail.com>
2026-08-13 06:07:41 +0530

    Valid palindrome 2

---
+++ Strings/validPalindrome2.java
@@ -0,0 +1,27 @@
+class validPalindrome2 {
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return isPalindrome(left+1,right,s) || isPalindrome(left,right-1,s);
+            }
+            left++;
+            right--;   //remember example of 'abc' is false
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(int left,int right,String s){
+
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
