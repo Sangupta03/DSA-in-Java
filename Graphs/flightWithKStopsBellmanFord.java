a08a09908c2920568c2d731755351d99ac401074
Sangupta03 <sanjoligupta11d@gmail.com>
2026-08-29 12:39:35 +0530

    Cheapest flight with k stops: bellman ford variant

--- Graphs/flightWithKStopsBellmanFord.java
+++ Graphs/flightWithKStopsBellmanFord.java
@@ -1,7 +1,7 @@
 package Graphs;
 import java.util.*;
 
-class flightWithKStops {
+class flightWithKStopsBellmanFord {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int[] dist=new int[n];
         Arrays.fill(dist,(int)1e9);
@@ -52,5 +52,34 @@ class Pair{
         this.dist=dist;
         this.node=node;
         this.stops=stops;
+    }
+}
+
+class Solution {
+    public static int findCheapestCost(int n, int[][] edges, int src, int dst, int k) {
+        // code here
+        ArrayList<ArrayList<int[]>> adj=new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] dist=new int[n];
+        
+        Arrays.fill(dist,(int)1e9);
+        dist[src]=0;
+        
+        for(int i=0;i<=k;i++){ //k+1
+            int[] temp=dist.clone();
+            for(int[] edge:edges){
+                int u=edge[0];
+                int v=edge[1];
+                int w=edge[2];
+                if(dist[u]!=(int)1e9 && dist[u]+w<temp[v]){
+                    temp[v]=w+dist[u];
+                }
+            }
+            dist=temp;
+        }
+        return dist[dst]==(int)1e9?-1:dist[dst];
     }
 }
