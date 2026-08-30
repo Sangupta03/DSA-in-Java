52ff5b51e319aa41ae39908d70e1ac529afe465a
Sangupta03 <sanjoligupta11d@gmail.com>
2026-08-30 16:18:14 +0530

    Floyd Warshall

---
+++ Graphs/floydWarshall.java
@@ -0,0 +1,18 @@
+package Graphs;
+
+class floydWarshall {
+    public void floydWarshallQ(int[][] dist) {
+     
+        int n=dist.length;
+        
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][via]!=1e8 && dist[via][j]!=1e8){
+                        dist[i][j]=Math.min(dist[i][j],dist[i][via]+dist[via][j]);
+                    }
+                }
+            }
+        }
+    }
+}
