7ac7f5368fd6610f8b6c723c7062e00f1bb408dd
Sangupta03 <sanjoligupta11d@gmail.com>
2026-09-07 04:15:01 +0530

    Distinct subsequences

--- dp/distinctSubsequence.java
+++ dp/distinctSubsequence.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class distinctSubsequence {
     public int numDistinct(String s, String t) {
         int n=s.length();
@@ -21,5 +22,33 @@ class distinctSubsequence {
             prev=temp;
         }
         return prev[m]; 
+    }
+}
+
+//memo
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+
+        int[][] dp=new int[n][m];
+        for(int[] oned:dp){
+            Arrays.fill(oned,-1);
+        }
+
+        return solve(n-1,m-1,s,t,dp);
+    }
+
+    //coin change family problem
+    public int solve(int i,int j,String s,String t,int[][] dp){
+        if(j<0) return 1; //entire t is found
+        if(i<0) return 0; //entire s is processed
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
+        }
+        return dp[i][j]=solve(i-1,j,s,t,dp);
     }
 }
