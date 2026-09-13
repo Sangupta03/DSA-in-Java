3a8718fa1cea71a9a895e4bb69c4581d3a80e979
Sangupta03 <sanjoligupta11d@gmail.com>
2026-09-13 18:10:08 +0530

    LCA of BST

---
+++ BST/LCAofBST.java
@@ -0,0 +1,14 @@
+package BST;
+
+class LCAofBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return root;
+        if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }else if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }else{
+            return root;
+        }
+    }
+}
